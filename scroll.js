a7c7a8eb352fec8d38e8c126a542f02d0e60dca5
Allisson-Lima-Dev <allysoncosta@outlook.com>
2021-05-01 15:45:39 -0300

    v.1

---
+++ scroll.js
@@ -0,0 +1,17 @@
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+
+function animeScroll(){
+const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4
+target.forEach(function(element){
+if((windowTop)> element.offsetTop){
+    element.classList.add(animationClass);
+}else{ element.classList.remove(animationClass);
+}
+})
+}
+animeScroll();
+window.addEventListener('scroll', function(){
+    animeScroll();
+})
